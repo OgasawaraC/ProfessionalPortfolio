b162422fa9bc632f5e47ec4ac701ed26ccf3ea64
OgasawaraC <caueoga@gmail.com>
2023-04-25 00:53:33 -0300

    Backup

---
+++ my-portfolio/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <div>
+            {/* Social Icons */}
+        </div>
+    </header>
+  )
+}
+
+export default Header;
---
+++ my-portfolio/pages/index.tsx
@@ -0,0 +1,31 @@
+import Header from '@/components/header';
+
+
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return(
+    <div>
+      <Head>
+        <title>Ogasawara's Portfolio</title>
+      </Head>
+
+      {/* Header */}
+        <Header />
+      {/* Hero */}
+
+      {/*About*/}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact */}
+    </div>
+  );
+};
+
+export default Home;
